302b48011c7c4dd8e6fb0910111b932128ab7ca3
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-10-28 14:01:34 +1100

    Initialise React.js app

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Nonverse | SecureAuth</title>
+</head>
+<body>
+<div id="root">
+    {{--Div in which React.js app will be rendered--}}
+</div>
+<script defer src="../js/app.js"></script>
+</body>
+</html>
