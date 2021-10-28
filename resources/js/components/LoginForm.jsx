eb7a8e14157ac2ab8fb2fe5ac50b97d6d3385752
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-10-29 00:55:37 +1100

    Create static login form

---
+++ resources/js/components/LoginForm.jsx
@@ -0,0 +1,32 @@
+import React, {useState} from "react";
+
+import Form from "../elements/Form";
+import Field from "../elements/Field";
+
+const LoginForm = ({load}) => {
+
+    const [email, setEmail] = useState('')
+
+    function submit() {
+        load(true)
+        console.log("Test Component Post Success")
+
+        setTimeout(() => {
+            load(false)
+        }, 1200)
+    }
+
+    return (
+        <div className="content-wrapper">
+            <Form title={"Login To Continue"} submit={submit}>
+                <Field update={setEmail} name={"Email"} value={email}/>
+            </Form>
+            <div className="links">
+                <a href="#">Forgot Your Email?</a>
+                <a href="#">Create Account</a>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm
