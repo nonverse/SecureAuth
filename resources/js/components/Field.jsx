c3cac717dcdfa2ef8a56605ad50131c8cd2f1ae3
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-10-29 23:31:26 +1100

    Make error handling non-compulsory

--- resources/js/components/Field.jsx
+++ resources/js/components/Field.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import {Field as FormikField} from "formik";
 
-const Field = ({name, placeholder, errors, validate}) => {
+const Field = ({password, name, placeholder, errors, validate}) => {
 
-    let error = errors[name]
+    let error
+    if (errors) {
+        error = errors[name]
+    }
     let validator
     if (validate) {
         validator = validate
@@ -13,7 +16,8 @@ const Field = ({name, placeholder, errors, validate}) => {
 
     return (
         <div className="field-wrapper">
-            <FormikField className={error ? 'field-error' : ''} id={name} name={name} placeholder={placeholder}
+            <FormikField type={password ? 'password' : 'text'} className={error ? 'field-error' : ''} id={name}
+                         name={name} placeholder={placeholder}
                          validate={validator}/>
             {error ? <span className="error">{error}</span> : ''}
         </div>
