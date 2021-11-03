d623665ba4950b0e222cd44cfe3019bc5864694b
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-11-03 22:02:55 +1100

    Create 2FA form

--- resources/js/components/Login/LoginForm.jsx
+++ resources/js/components/Login/LoginForm.jsx
@@ -2,11 +2,12 @@ import React, {useState} from "react";
 import Email from "./Email";
 import Password from "./Password";
 import ProgressiveForm from "../ProgressiveForm";
+import TwoFactorCheckpoint from "./TwoFactorCheckpoint";
 
 const LoginForm = ({load}) => {
 
     const [user, updateUser] = useState({})
-    const [state, setState] = useState(1)
+    const [state, setState] = useState(3)
 
     function advance() {
         setState(state + 1)
@@ -19,7 +20,8 @@ const LoginForm = ({load}) => {
     return (
         <ProgressiveForm state={state} views={{
             1: <Email load={load} updateUser={updateUser} advance={advance}/>,
-            2: <Password load={load} user={user} advance={advance} back={previous}/>
+            2: <Password load={load} user={user} advance={advance} back={previous}/>,
+            3: <TwoFactorCheckpoint load={load} userData={user}/>
         }}/>
     )
 }
---
+++ resources/js/components/Login/TwoFactorCheckpoint.jsx
@@ -0,0 +1,45 @@
+import React, {useState} from "react";
+import {useHistory} from "react-router-dom";
+import {Formik} from "formik";
+import Form from "../elements/Form";
+import Field from "../elements/Field";
+import validate from "../../scripts/validate";
+
+const TwoFactorCheckpoint = ({load, userData}) => {
+
+    const history = useHistory()
+    const [error, setError] = useState('')
+
+    async function submit(values) {
+        //
+    }
+
+    function validateOtp(value) {
+        setError('');
+        return validate.otp(value)
+    }
+
+    return (
+        <div className="content-wrapper">
+            <h4>Two Factor Authentication</h4>
+            <span>Your account is protected by 2FA</span>
+            <Formik initialValues={{
+                otp: ''
+            }} onSubmit={() => {
+                submit(values)
+            }}>
+                {({errors}) => (
+                    <Form submitCta={"Verify"}>
+                        <Field placeholder={"Code"} validate={validateOtp} name={"otp"} error={errors.otp ? errors.otp : error}/>
+                    </Form>
+                )}
+            </Formik>
+            <div className="links">
+                <span className="link-btn">Can't access authenticator?</span>
+                <span className="link-btn" onClick={() => window.location.reload()}>Restart login</span>
+            </div>
+        </div>
+    )
+}
+
+export default TwoFactorCheckpoint
