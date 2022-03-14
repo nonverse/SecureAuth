646bfe686613e7d2f9c319d631ddf397d1dc4282
isuru <isuru2003a@gmail.com>
2022-03-14 19:12:37 +1100

    Create controller and method to verify an incoming first party API access token

---
+++ app/Http/Controllers/Api/FirstPartyTokenController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Contracts\Repository\UserRepositoryInterface;
+use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Encryption\Encrypter;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class FirstPartyTokenController extends Controller
+{
+    /**
+     * @var UserRepositoryInterface
+     */
+    private $repository;
+
+    /**
+     * @var Encrypter
+     */
+    private $encrypter;
+
+    public function __construct(
+        UserRepositoryInterface $repository,
+        Encrypter               $encrypter
+    )
+    {
+        $this->repository = $repository;
+        $this->encrypter = $encrypter;
+    }
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function verify(Request $request): JsonResponse
+    {
+        $request->validate([
+            'token' => 'required|min:64|max:64'
+        ]);
+
+        $user = $request->user();
+        $token = $this->encrypter->decryptString($user->api_encryption);
+
+        if ($request->input('token') !== $token) {
+            return new JsonResponse([
+                'errors' => [
+                    'token' => 'Invalid API token'
+                ]
+            ], 401);
+        }
+
+        return new JsonResponse([
+            'data' => [
+                'uuid' => $user->uuid,
+                'authenticated' => true
+            ]
+        ]);
+    }
+}
