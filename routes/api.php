a53cfbf46f20a7b57ec67344e03c3a5e7a27d69b
isuru <isuru2003a@gmail.com>
2022-03-14 20:20:07 +1100

    Create API authentication route

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| CORS and auth protected user API routes
+| These routes can only be access by apps on the .nonverse.net domain by
+| a session authenticated user
+|--------------------------------------------------------------------------
+| Endpoint: /api
+|
+*/
+
+// First party front end API authentication
+Route::post('/authenticate', [\App\Http\Controllers\Api\FirstPartyTokenController::class, 'verify']);
