8e410ac717fc43fdb5c720c04d388f8424a466df
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-14 20:59:17 +1000

    Create Fluid component for forms

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Fluid from "./elements/Fluid";
 import Logo from "./elements/Logo";
 
 function Index() {
     return (
         <div className="container">
             <Logo color={"#333344"}/>
+            <Fluid/>
         </div>
     );
 }
---
+++ resources/js/components/elements/Fluid.jsx
@@ -0,0 +1,12 @@
+const Fluid = () => {
+
+    return (
+        <div className="fluid-container">
+            <div className="fluid">
+
+            </div>
+        </div>
+    )
+}
+
+export default Fluid;
