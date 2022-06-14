318b448bbd9ed12093c28a2ff53554d72fb7dd42
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-14 22:20:33 +1000

    Formatting and cleanup

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Fluid from "./elements/Fluid";
 import Logo from "./elements/Logo";
 import {BrowserRouter} from "react-router-dom";
 import Router from "./Router";
 
 function Index() {
+
     return (
         <div className="container">
             <Logo color={"#333344"}/>
