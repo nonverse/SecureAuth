5da26bf61bdf051c22491af175865b39d86ed862
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-15 22:42:22 +1000

    Allow label and change button to be passed into field component

--- resources/js/components/elements/Field.jsx
+++ resources/js/components/elements/Field.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import {Field as FormikField} from "formik";
+import LinkButton from "./LinkButton";
 
-const Field = ({password, name, placeholder, value, error, validate}) => {
+const Field = ({password, name, label, placeholder, value, error, validate, change}) => {
 
     let validator
     if (validate) {
@@ -12,11 +13,13 @@ const Field = ({password, name, placeholder, value, error, validate}) => {
 
     return (
         <div className="field-wrapper">
+            <span className={"op-05"}>{label}</span>
             <FormikField type={password ? 'password' : 'text'} className={error ? 'field-error' : ''} id={name}
                          name={name} placeholder={placeholder}
                          value={value}
                          validate={validator}/>
             {error ? <span className="error">{error}</span> : ''}
+            {change ? (<LinkButton action={change}>Change</LinkButton>) : ''}
         </div>
     )
 
