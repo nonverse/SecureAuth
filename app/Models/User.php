d036fb55a3d1bd00b9394d445a63cd885acf1a75
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-19 22:57:29 +1000

    Update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,55 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+
+/**
+ * @property string $uuid
+ * @property string $username
+ * @property string $name_first
+ * @property string $name_last
+ * @property string $email
+ * @property string $password
+ * @property bool $admin
+ * @property bool $use_totp
+ * @property string $totp_secret
+ * @property string $violations
+ * @property Carbon $email_verified_at
+ * @property Carbon $totp_authenticated_at
+ * @property Carbon $violation_ends_at
+ * @property string $remember_token
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ */
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uuid';
+
+    /**
+     * Indicates if the model's ID is auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The data type of the auto-incrementing ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
 
     /**
      * The attributes that are mass assignable.
@@ -18,9 +58,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        //
     ];
 
     /**
