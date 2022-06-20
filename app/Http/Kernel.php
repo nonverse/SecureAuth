a898d3c78ff4a45896a1afc02341f00cf1fd1619
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-20 23:33:44 +1000

    Create middleware to direct users to login if a user cookie is found

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\NoUserCookie;
 use App\Http\Middleware\UserCookie;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
@@ -65,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'usercookie' => UserCookie::class,
+        'nousercookie' => NoUserCookie::class,
     ];
 }
