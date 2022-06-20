6b9fe7be90cea545f18accf58dad6568b220f7b4
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-20 22:42:53 +1000

    Create a user short-lived, unauthenticated cookie when initialising email. This cookie will last 5 minutes and will allow the password form to be refreshed without having to re-enter the email

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\User;
 
 use App\Contracts\Repository\UserRepositoryInterface;
 use App\Http\Controllers\Controller;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
@@ -47,12 +46,46 @@ class UserController extends Controller
         }
 
         /*
+         * Create an unauthenticated browser cookie containing the user's UUID
+         * This cookie expires in 5 minutes
+         */
+        $cookie = cookie('user', json_encode([
+            'uuid' => $user->uuid,
+            'has_authed' => false
+        ]), 5);
+
+        /*
          * If an account is found, return the user's UUID,
          * first name and last name
          */
+        return response()->json([
+            'data' => [
+                'uuid' => $user->uuid,
+                'name_first' => $user->name_first,
+                'name_last' => $user->name_last,
+            ]
+        ])->withCookie($cookie);
+    }
+
+    /**
+     * Return details of a user stored in browser cookie
+     *
+     * @param Request $request
+     * @return JsonResponse|Response
+     */
+    public function cookie(Request $request): Response|JsonResponse
+    {
+
+        if (!$request->cookie('user')) {
+            return response('No user cookie found', 404);
+        }
+
+        $user = $this->repository->get(json_decode($request->cookie('user'))->uuid);
+
         return new JsonResponse([
             'data' => [
                 'uuid' => $user->uuid,
+                'email' => $user->email,
                 'name_first' => $user->name_first,
                 'name_last' => $user->name_last,
             ]
