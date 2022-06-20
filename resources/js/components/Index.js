26b340b5a479e92c234076985b348fd1d9377289
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-20 22:45:24 +1000

    Implement app loader

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import Logo from "./elements/Logo";
 import {BrowserRouter} from "react-router-dom";
 import Router from "./Router";
+import Loader from "./Loader";
 
 function Index() {
+
+    const[initialized, setInitialized] = useState(false)
 
     return (
         <div className="container">
             <Logo color={"#333344"}/>
             <BrowserRouter>
-                <Router/>
+                <Router setInitialized={setInitialized}/>
             </BrowserRouter>
+            {initialized ? '' : <Loader/>}
         </div>
     );
 }
