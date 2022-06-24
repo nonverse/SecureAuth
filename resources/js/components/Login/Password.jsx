635c8eac8ecb0ed242fbf75fcbb8b4520723bec9
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-24 22:59:55 +1000

    Create submit logic for login password form

--- resources/js/components/Login/Password.jsx
+++ resources/js/components/Login/Password.jsx
@@ -5,16 +5,53 @@ import validate from "../../../scripts/validate";
 import LinkButton from "../elements/LinkButton";
 import {useNavigate} from "react-router-dom";
 import {auth} from "../../../scripts/api/auth";
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {endLoad, startLoad} from "../../state/load";
 
-const Password = ({user, setUser, advance}) => {
+const Password = ({user, setUser, setInitialized, intended, advance}) => {
 
+    const [error, setError] = useState('')
+    const dispatch = useDispatch()
     const navigate = useNavigate()
 
     async function submit(values) {
-        setUser({
-            ...user,
+
+        dispatch(startLoad())
+
+        await auth.post('login', {
+            email: user.email,
             password: values.password
         })
+            .then((response) => {
+                if (response.data.data.complete) {
+                    dispatch(endLoad())
+                    setInitialized(false)
+                    window.location.replace(`http://${intended.host}${intended.resource}`)
+                } else if (response.data.data.authentication_token) {
+                    setUser({
+                        ...user,
+                        authentication_token: response.data.data.authentication_token
+                    })
+                    dispatch(endLoad())
+                    advance()
+                }
+            })
+            .catch((e) => {
+                switch (e.response.status) {
+                    case 401:
+                        setError('Password is incorrect')
+                        break;
+                    default:
+                        setError('Something went wrong')
+                }
+                dispatch(endLoad())
+            })
+    }
+
+    function validatePassword(value) {
+        setError('')
+        return validate.require(value)
     }
 
     return (
@@ -38,8 +75,8 @@ const Password = ({user, setUser, advance}) => {
             }}>
                 {({errors}) => (
                     <Form>
-                        <Field password name={"password"} placeholder={"Enter Your Password"} error={errors.password}
-                               validate={validate.require}/>
+                        <Field doesLoad password name={"password"} placeholder={"Enter Your Password"} error={errors.password ? errors.password : error}
+                               validate={validatePassword}/>
                     </Form>
                 )}
             </Formik>
