26ec4a805062c7c25bb3227e3676e32c3a9ce4bd
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-24 22:59:38 +1000

    Store intended host and resource in application state on load of login form

--- resources/js/components/Login/Login.jsx
+++ resources/js/components/Login/Login.jsx
@@ -3,10 +3,17 @@ import ProgressiveForm from "../elements/ProgressiveForm";
 import Password from "./Password";
 import TwoFactor from "./TwoFactor";
 import {auth} from "../../../scripts/api/auth";
+import {useSelector} from "react-redux";
 
 const Login = ({user, setUser, setInitialized}) => {
 
     const [state, setState] = useState(1)
+    const load = useSelector((state) => state.loader.value)
+    const query = new URLSearchParams(window.location.search)
+    const intended = {
+        host: query.get('host'),
+        resource: query.get('resource')
+    }
 
     useEffect(async () => {
         await auth.get('api/user/cookie')
@@ -30,13 +37,16 @@ const Login = ({user, setUser, setInitialized}) => {
     }
 
     return (
-        <ProgressiveForm
-            state={state}
-            forms={{
-                1: <Password user={user} setUser={setUser} setInitialized={setInitialized} advance={advance}/>,
-                2: <TwoFactor user={user} setUser={setUser}/>
-            }}
-        />
+        <div className={load ? 'form-loading action-cover op-05' : ''}>
+            <ProgressiveForm
+                state={state}
+                forms={{
+                    1: <Password user={user} setUser={setUser} setInitialized={setInitialized} intended={intended}
+                                 advance={advance}/>,
+                    2: <TwoFactor user={user} setUser={setUser} setInitialized={setInitialized} intended={intended}/>
+                }}
+            />
+        </div>
     )
 }
 
