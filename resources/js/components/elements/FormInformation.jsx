910230601a626d366e012d91233880bce0efbbab
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-25 12:48:28 +1000

    Add option to close form info

--- resources/js/components/elements/FormInformation.jsx
+++ resources/js/components/elements/FormInformation.jsx
@@ -1,16 +1,20 @@
 import {motion} from "framer-motion";
+import LinkButton from "./LinkButton";
 
-const FormInformation = ({weight, id, children}) => {
+const FormInformation = ({weight, id, close, children}) => {
 
     return (
-        <motion.div className={`form-info form-info-${weight}`}
+        <motion.div className="form-info-wrapper"
                     id={id}
                     initial={{opacity: 0}}
                     animate={{opacity: 1}}
                     exit={{opacity: 0}}
                     transition={{duration: .5}}
         >
-            {children}
+            <div className={`form-info form-info-${weight}`}>
+                {children}
+            </div>
+            {close ? <LinkButton action={close}>Close</LinkButton> : ''}
         </motion.div>
     )
 }
