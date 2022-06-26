548e7da8b3bbb1cf62eacc64a528b6d4f4918e50
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-26 15:22:24 +1000

    Add logic to post 2FA code to backend and verify user

--- resources/js/components/Login/TwoFactor.jsx
+++ resources/js/components/Login/TwoFactor.jsx
@@ -3,11 +3,48 @@ import {Formik} from "formik";
 import Form from "../elements/Form";
 import Field from "../elements/Field";
 import validate from "../../../scripts/validate";
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {endLoad, startLoad} from "../../state/load";
+import {auth} from "../../../scripts/api/auth";
 
-const TwoFactor = ({user, setUser}) => {
+const TwoFactor = ({user, setUser, setInitialized, intended}) => {
+
+    const [error, setError] = useState('')
+    const dispatch = useDispatch()
 
     async function submit(values) {
-        //
+        dispatch(startLoad())
+
+        await auth.post('login/two-factor', {
+            one_time_password: values.code,
+            authentication_token: user.authentication_token
+        })
+            .then((response) => {
+                if (response.data.data.complete) {
+                    dispatch(endLoad())
+                    setInitialized(false)
+                    window.location.replace(`http://${intended.host}${intended.resource}`)
+                }
+            })
+            .catch((e) => {
+                switch (e.response.status) {
+                    case 422:
+                        setError('Something went wrong')
+                        break
+                    case 500:
+                        setError('Something went wrong')
+                        break
+                    default:
+                        setError(e.response.data)
+                }
+                dispatch(endLoad())
+            })
+    }
+
+    function validateCode(value) {
+        setError('')
+        return validate.require(value, 6, 6)
     }
 
     return (
@@ -24,7 +61,8 @@ const TwoFactor = ({user, setUser}) => {
             }}>
                 {({errors}) => (
                     <Form>
-                        <Field password name={"code"} placeholder={"2FA Code"} error={errors.code} validate={validate.otp}/>
+                        <Field doesLoad name={"code"} placeholder={"2FA Code"} error={errors.code ? errors.code : error}
+                               validate={validateCode}/>
                     </Form>
                 )}
             </Formik>
