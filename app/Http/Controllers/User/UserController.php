169895d96dfdf72aab2da0b41a848e330aff559f
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-06-26 18:26:04 +1000

    Return if user has TOTP enabled

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -47,7 +47,8 @@ class UserController extends Controller
             'data' => [
                 'uuid' => $request->user()->uuid,
                 'name_first' => $request->user()->name_first,
-                'name_last' => $request->user()->name_last
+                'name_last' => $request->user()->name_last,
+                'use_totp' => $request->user()->use_totp
             ]
         ]);
     }
