511986287f1414fc9e8886734706d5f9909e5b39
Isuru Abhayaratne <isuru2003a@gmail.com>
2022-07-14 17:03:31 +1000

    Remove unused functions from input validation script

--- resources/scripts/validate.js
+++ resources/scripts/validate.js
@@ -1,42 +1,6 @@
 import axios from "axios";
 
 class validate {
-    constructor() {
-
-        // API target endpoint
-        this.url = 'https://api.nonverse.net/validator/';
-
-        // Config
-        axios.defaults.withCredentials = true;
-    }
-
-    async validateNewEmail(email) {
-        return await axios.post(
-            `${this.url}validate-new-email`,
-            {
-                email: email,
-            }
-        )
-    }
-
-    async validateNewUser(username) {
-        return await axios.post(
-            `${this.url}validate-new-user`,
-            {
-                username: username,
-            }
-        )
-    }
-
-    async validateNewProfile(mc_username) {
-        return await axios.post(
-            `${this.url}profile`,
-            {
-                mc_username: mc_username,
-            }
-        )
-    }
-
     email(value) {
         let error;
         if (!value) {
@@ -72,29 +36,6 @@ class validate {
         }
         if (value !== compare) {
             error = 'Confirmation does not match'
-        }
-        return error
-    }
-
-    otp(value) {
-        let error;
-        if (!value) {
-            error = "A one time code is required"
-        } else if (!/^[0-9]*$/i.test(value)) {
-            error = "Your code only contains digits"
-        } else if (value.length !== 6) {
-            error = "Your code must only be 6 digits"
-        }
-        return error
-    }
-
-    max(value, length) {
-        let error;
-        if (!value) {
-            error = "This field is required"
-        }
-        if (value.length > length) {
-            error = `Maximum ${length} characters`
         }
         return error
     }
