d18720a95d5943fdb1485236a93f17d74b435600
Isuru Abhayaratne <isuru2003a@gmail.com>
2023-01-21 21:28:40 +1100

    Check if user exists for the UUID stored in user cookie. This fixes a bug where the UI would not load any forms if a user does not exist for the stored UUID. (e.g. The user has been deleted or suspended etc...)

--- app/Http/Middleware/NoUserCookie.php
+++ app/Http/Middleware/NoUserCookie.php
@@ -2,24 +2,39 @@
 
 namespace App\Http\Middleware;
 
+use App\Contracts\Repository\UserRepositoryInterface;
 use Closure;
 use Illuminate\Http\Request;
 
 class NoUserCookie
 {
     /**
+     * @var UserRepositoryInterface
+     */
+    private UserRepositoryInterface $repository;
+
+    public function __construct(
+        UserRepositoryInterface $userRepository
+    )
+    {
+        $this->repository = $userRepository;
+    }
+
+    /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next)
     {
-        if ($request->cookie('user')) {
+        $cookie = $request->cookie('user');
+
+        if ($cookie && $this->repository->get(json_decode($cookie, true)['uuid'])) {
             $intended = [
                 'host' => urlencode($request->input('host') ?: urlencode(env('BASE_APP_URL'))),
-                'resource' => urlencode($request->input('resource')  ?: urlencode('/'))
+                'resource' => urlencode($request->input('resource') ?: urlencode('/'))
             ];
 
             return redirect('/login?host=' . $intended['host'] . '&resource=' . $intended['resource']);
