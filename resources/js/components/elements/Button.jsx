6434e8768c1a7876844ee9e17ba3263119fdf825
Isuru Abhayaratne <isuru2003a@gmail.com>
2023-02-02 04:26:19 +1100

    Allow action to be passed into button

--- resources/js/components/elements/Button.jsx
+++ resources/js/components/elements/Button.jsx
@@ -1,8 +1,13 @@
-const Button = ({submit, children}) => (
-
-    <button className={"button"} type={submit ? 'submit' : 'button'}>
-        {children}
-    </button>
-)
+const Button = ({submit, action, children}) => {
+    return (
+        <button className={"button"} type={submit ? 'submit' : 'button'} onClick={() => {
+            if (!submit) {
+                action()
+            }
+        }}>
+            {children}
+        </button>
+    )
+}
 
 export default Button;
