f3c838d3451d32a77c7565b45780ed1ff5022459
Isuru Abhayaratne <isuru2003a@gmail.com>
2023-02-10 02:17:33 +1100

    Add logic to post authorization query to backend and get an authorization code

--- resources/js/components/OAuth/OAuthAuthorize.jsx
+++ resources/js/components/OAuth/OAuthAuthorize.jsx
@@ -1,58 +1,56 @@
 import {useEffect, useState} from "react";
-import LinkButton from "../elements/LinkButton";
-import Button from "../elements/Button";
 import FormInformation from "../elements/FormInformation";
 import {auth} from "../../../scripts/api/auth";
+import OAuthClientInformation from "./OAuthClientInformation";
+import {useDispatch} from "react-redux";
+import {startLoad} from "../../state/load";
 
 const OAuthAuthorize = ({user, setInitialized}) => {
 
-    const [request, setRequest] = useState({
-        client_name: 'Pinterest',
-        scopes: [
-            'View your email address',
-            'View your basic information',
-        ]})
+    const [response, setResponse] = useState({})
+    const query = new URLSearchParams(window.location.search)
+    const dispatch = useDispatch()
 
-    useEffect(() => {
-        setInitialized(true)
+    useEffect(async () => {
+        await auth.post('api/oauth/authorize', null, {params: query})
+            .then((response) => {
+                if (response.data.data.redirect_uri) {
+                    window.location.replace(response.data.data.redirect_uri)
+                } else {
+                    setResponse(response.data.data)
+                    setInitialized(true)
+                }
+            })
+            .catch((e) => {
+                setResponse(e.response.data)
+                setInitialized(true)
+            })
+
     }, [])
 
-    async function handle(approve) {
-        await auth.post('oauth/authorize', {
+    async function submit(approve) {
+        const params = Object.fromEntries(query)
 
+        setInitialized(false)
+
+        await auth.post('oauth/authorize', {
+            state: params.state,
+            client_id: params.client_id,
+            auth_token: response.auth_token,
+            _method: approve ? null : 'delete'
         })
+            .then((response) => {
+                window.location.replace(response.data.data.redirect_uri)
+            })
     }
 
     return (
         <>
-            <div className="fluid-text">
-                <span>{request.client_name}</span>
-                <h1>Would like to access your account</h1>
-                <LinkButton action={() => {
-                    //
-                }}>Use a different account</LinkButton>
-            </div>
-            <div className="fluid-body">
-                <h1><span className="splash">{request.client_name}</span> will be able to</h1>
-                <ul>
-                    {request.scopes.map(scope => (
-                        <li key={scope.replace(/\s+/g, '-').toLowerCase()}>{scope}</li>
-                    ))}
-                </ul>
-            </div>
-            <FormInformation weight={'warning'}>
-                Please ensure that you trust <span className="splash">{request.client_name}</span>
-                <br/>
-                Make sure you read their privacy policy to find out how your data will be handled as you may be sharing sensitive information with this application
-                <br/>
-            </FormInformation>
-            <FormInformation weight={'default'}>
-                You can always see and manage connections in your <a className="link" href="https://my.nonverse.net/account/apps" rel="noreferrer" target="_blank">Account Applications</a> page
-            </FormInformation>
-            <div className="fluid-actions">
-                <LinkButton>Deny</LinkButton>
-                <Button>Approve</Button>
-            </div>
+            {response.error ? (
+                <FormInformation weight={'danger'}>{response.error_description}</FormInformation>
+            ) : (
+                <OAuthClientInformation response={response} submit={submit}/>
+            )}
         </>
     )
 }
