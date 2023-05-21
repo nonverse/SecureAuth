d1a29017c5658ec5d040a61dc250efe8696a9fc3
isuru <isuru2003a@gmail.com>
2023-05-21 22:21:40 +0530

    Setup application state

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import Logo from "../elements/Logo";
 import {BrowserRouter} from "react-router-dom";
 import Loader from "./Loader";
+import Email from "./Email";
+import {Provider} from "react-redux";
+import store from "../state/store";
 
 function Index() {
 
@@ -15,7 +18,7 @@ function Index() {
                     <Logo/>
                     <BrowserRouter>
                         <div className="container">
-
+                            <Email/>
                         </div>
                     </BrowserRouter>
                 </>
@@ -29,7 +32,9 @@ export default Index;
 
 if (document.getElementById('root')) {
     ReactDOM.render(
-        <Index/>
+        <Provider store={store}>
+            <Index/>
+        </Provider>
         , document.getElementById('root')
     );
 }
---
+++ resources/js/state/store.js
@@ -0,0 +1,8 @@
+import {combineReducers, configureStore} from "@reduxjs/toolkit";
+import userReducer from "../state/user";
+
+export default configureStore({
+    reducer: {
+        user: userReducer,
+    },
+})
