7859c304b8f58b400f026148060fc37638f42c8e
isuru <isuru2003a@gmail.com>
2023-05-22 17:02:05 +0530

    Show errors if post errors

--- resources/js/components/Login/Password.jsx
+++ resources/js/components/Login/Password.jsx
@@ -2,7 +2,7 @@ import Fluid from "../Fluid";
 import {useDispatch, useSelector} from "react-redux";
 import {Formik} from "formik";
 import Form from "../../elements/Form";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import Field from "../../elements/Field";
 import validate from "../../scripts/validate";
 import InLineButton from "../../elements/InLineButton";
@@ -13,11 +13,17 @@ import {updateUser} from "../../state/user";
 const Password = ({advance}) => {
 
     const user = useSelector(state => state.user.value)
+    const [error, setError] = useState('')
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(updateLoader(false))
     }, [])
+
+    function validatePassword(value) {
+        setError('')
+        return validate.require(value)
+    }
 
     return (
         <Fluid heading={`Welcome back, ${user.name_first}`} subHeading="What's your password?">
@@ -40,11 +46,21 @@ const Password = ({advance}) => {
                             advance()
                         }
                     })
+                    .catch(e => {
+                        switch (e.response.status) {
+                            case 401:
+                                setError('Password is incorrect')
+                                break
+                            default:
+                                setError('Something went wrong')
+                        }
+                        dispatch(updateLoader(false))
+                    })
             }}>
                 {({errors}) => (
                     <Form id="fluid-form" cta="Continue">
-                        <Field password name="password" label="Password" validate={validate.require}
-                               error={errors.password}/>
+                        <Field password name="password" label="Password" validate={validatePassword}
+                               error={errors.password ? errors.password : error}/>
                         <div className="fluid-actions">
                             <InLineButton id="forgot-password">Forgot Password</InLineButton>
                             <InLineButton id="not-you">Not you?</InLineButton>
