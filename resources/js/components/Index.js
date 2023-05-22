daa2ae430f8f1ecbe90569140a8f3266bfbb01e8
isuru <isuru2003a@gmail.com>
2023-05-22 19:06:32 +0530

    Add check for register page. If user goes to register page directly, redirect to email starting point, unless a valid email is provided in the query string

--- resources/js/components/Index.js
+++ resources/js/components/Index.js
@@ -8,11 +8,13 @@ import store from "../state/store";
 import Router from "./Router";
 import {auth} from "../scripts/api/auth";
 import {updateUser} from "../state/user";
+import validate from "../scripts/validate";
 
 function Index() {
 
     const [initialised, setInitialised] = useState(false)
     const loading = useSelector(state => state.loader.value)
+    const query = new URLSearchParams(window.location.search)
     const dispatch = useDispatch()
 
     useEffect(async () => {
@@ -22,6 +24,15 @@ function Index() {
                     dispatch(updateUser(response.data.data))
                     setInitialised(true)
                 })
+        } else if (window.location.pathname === '/register') {
+            if (validate.email(query.get('email'))) {
+                window.location.replace('/')
+            } else {
+                dispatch(updateUser({
+                    email: query.get('email')
+                }))
+            }
+            setInitialised(true)
         } else {
             setInitialised(true)
         }
