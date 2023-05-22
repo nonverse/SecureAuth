16a0d8b5e15018c36e05074c50e4336ace058f08
isuru <isuru2003a@gmail.com>
2023-05-22 13:07:11 +0530

    Start loader when submitting password

--- resources/js/components/Login/Password.jsx
+++ resources/js/components/Login/Password.jsx
@@ -2,7 +2,7 @@ import Fluid from "../Fluid";
 import {useDispatch, useSelector} from "react-redux";
 import {Formik} from "formik";
 import Form from "../../elements/Form";
-import {useEffect, useState} from "react";
+import {useEffect} from "react";
 import Field from "../../elements/Field";
 import validate from "../../scripts/validate";
 import InLineButton from "../../elements/InLineButton";
@@ -12,26 +12,25 @@ import {updateLoader} from "../../state/loader";
 const Password = () => {
 
     const user = useSelector(state => state.user.value)
-    const [loading, setLoading] = useState(false)
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(updateLoader(false))
-    })
+    }, [])
 
     return (
         <Fluid heading={`Welcome back, ${user.name_first}`} subHeading="What's your password?">
             <Formik initialValues={{
                 password: ''
             }} onSubmit={async (values) => {
-                setLoading(true)
+                dispatch(updateLoader(true))
                 await auth.post('login', {
                     ...values,
                     email: user.email
                 })
             }}>
                 {({errors}) => (
-                    <Form id="fluid-form" loading={loading} cta="Continue">
+                    <Form id="fluid-form" cta="Continue">
                         <Field password name="password" label="Password" validate={validate.require}
                                error={errors.password}/>
                         <div className="fluid-actions">
