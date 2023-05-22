a91b64abd0d868c5dc62c5757adaefafda7644d6
isuru <isuru2003a@gmail.com>
2023-05-22 12:22:31 +0530

    Add fluid animations

--- resources/js/components/Fluid.jsx
+++ resources/js/components/Fluid.jsx
@@ -1,15 +1,23 @@
-const Fluid = ({heading, subheading, children}) => {
+import {motion} from "framer-motion";
+
+const Fluid = ({heading, subHeading, children}) => {
 
     return (
-        <div className="fluid">
+        <motion.div className="fluid"
+                    key={`${heading}-${subHeading}`}
+                    initial={{opacity: 0, x: 150}}
+                    animate={{opacity: 1, x: 0}}
+                    exit={{opacity: 0, x: -150}}
+                    transition={{duration: .1}}
+        >
             <div className="fluid-heading">
                 <h1>{heading}</h1>
-                <h2>{subheading}</h2>
+                <h2>{subHeading}</h2>
             </div>
             <div className="fluid-content">
                 {children}
             </div>
-        </div>
+        </motion.div>
     )
 }
 
