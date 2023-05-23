716910d051d49c9c3a885310071c6ec6e069c611
isuru <isuru2003a@gmail.com>
2023-05-23 13:08:01 +0530

    Create date of birth page

--- resources/js/components/Register/Register.jsx
+++ resources/js/components/Register/Register.jsx
@@ -2,19 +2,25 @@ import {useState} from "react";
 import Agreement from "./Agreement";
 import Details from "./Details";
 import Phone from "./Phone";
+import DateOfBirth from "./DateOfBirth";
 
 const Register = () => {
 
-    const [state, setState] = useState(2)
+    const [state, setState] = useState(3)
 
     const views = {
         0: <Agreement advance={advance}/>,
         1: <Details advance={advance}/>,
-        2: <Phone advance={advance}/>
+        2: <Phone advance={advance}/>,
+        3: <DateOfBirth advance={advance}/>,
     }
 
-    function advance() {
+    function advance(back) {
         setState(state + 1)
+
+        if (back) {
+            setState(state - 1)
+        }
     }
 
     return (
