ce9454d7ea065af2f24f7c1895c0bf2b6e541360
isuru <isuru2003a@gmail.com>
2023-05-24 08:02:19 +0530

    Create MessageBox component

---
+++ resources/js/components/MessageBox.jsx
@@ -0,0 +1,24 @@
+import InLineButton from "../elements/InLineButton";
+import {motion} from "framer-motion";
+
+const MessageBox = ({id, weight, onClose, children}) => {
+
+    return (
+        <motion.div className="message-box-container"
+                    key={id}
+                    initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0}}
+                    transition={{duration: .15}}>
+
+            <div className={`message-box message-box-${weight}`}>
+                {children}
+            </div>
+            {onClose ? <InLineButton onClick={() => {
+                onClose()
+            }}>Close</InLineButton> : ''}
+        </motion.div>
+    )
+}
+
+export default MessageBox
