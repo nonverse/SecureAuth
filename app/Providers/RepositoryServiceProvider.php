d177ef1f15c3bf57fe09e4f9969feacbfbf03a37
Isuru <isuru2003a@gmail.com>
2023-06-01 18:57:02 +1000

    Forgot to bind repositories

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace App\Providers;
 
+use App\Contracts\Repository\OAuth2\AccessTokenRepositoryInterface;
+use App\Contracts\Repository\OAuth2\AuthCodeRepositoryInterface;
+use App\Contracts\Repository\OAuth2\ClientRepositoryInterface;
+use App\Contracts\Repository\OAuth2\RefreshTokenRepositoryInterface;
 use App\Contracts\Repository\RepositoryInterface;
 use App\Contracts\Repository\UserRepositoryInterface;
+use App\Repositories\OAuth2\AccessTokenRepository;
+use App\Repositories\OAuth2\AuthCodeRepository;
+use App\Repositories\OAuth2\ClientRepository;
+use App\Repositories\OAuth2\RefreshTokenRepository;
 use App\Repositories\Repository;
 use App\Repositories\UserRepository;
 use Illuminate\Support\ServiceProvider;
@@ -17,7 +25,14 @@ class RepositoryServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
+        // Base
         $this->app->bind(RepositoryInterface::class, Repository::class);
         $this->app->bind(UserRepositoryInterface::class, UserRepository::class);
+
+        //OAuth2
+        $this->app->bind(ClientRepositoryInterface::class, ClientRepository::class);
+        $this->app->bind(AuthCodeRepositoryInterface::class, AuthCodeRepository::class);
+        $this->app->bind(AccessTokenRepositoryInterface::class, AccessTokenRepository::class);
+        $this->app->bind(RefreshTokenRepositoryInterface::class, RefreshTokenRepository::class);
     }
 }
