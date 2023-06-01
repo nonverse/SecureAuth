2aa27471dcca5fab9d5d9cf6422621536d02a8e1
Isuru <isuru2003a@gmail.com>
2023-06-01 16:52:11 +1000

    Remove laravel passport, I am going to do it myself the way i want

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,10 +24,5 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->registerPolicies();
-
-        Passport::hashClientSecrets();
-        Passport::tokensExpireIn(now()->addHour());
-        Passport::refreshTokensExpireIn(now()->addHours(24));
-        Passport::personalAccessTokensExpireIn(now()->addYear());
     }
 }
