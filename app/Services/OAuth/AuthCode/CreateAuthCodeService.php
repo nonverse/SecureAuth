9c5c785d04d43b69ad2b5cbe1effbf78b3ee93b7
Isuru <isuru2003a@gmail.com>
2023-06-01 22:40:53 +1000

    Create service to create Authorization Codes

---
+++ app/Services/OAuth/AuthCode/CreateAuthCodeService.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Services\OAuth\AuthCode;
+
+use App\Contracts\Repository\OAuth2\AuthCodeRepositoryInterface;
+use Carbon\CarbonImmutable;
+use Exception;
+use Firebase\JWT\JWT;
+use Illuminate\Contracts\Encryption\Encrypter;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CreateAuthCodeService
+{
+    /**
+     * @var AuthCodeRepositoryInterface
+     */
+    private AuthCodeRepositoryInterface $repository;
+
+    /**
+     * Create new auth code service
+     *
+     * @param AuthCodeRepositoryInterface $repository
+     */
+    public function __construct(
+        AuthCodeRepositoryInterface $repository,
+    )
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function handle(Request $request, array $data): string
+    {
+        $id = Str::random(100);
+
+        $payload = [
+            'iss' => env('APP_URL'),
+            'aud' => $request->input('redirect_uri'),
+            'nbf' => time(),
+            'iat' => time(),
+            'exp' => time() + 5 * 60,
+            'jti' => $id
+        ];
+
+        /**
+         * Create new auth code
+         */
+        $jwt = JWT::encode($payload, file_get_contents('./storage/oauth-private.key'), 'RS256');
+
+        /**
+         * Remove previous auth codes for user and client pair
+         */
+        $this->repository->update($this->repository->getUsingClientAndUser($data['client_id'], $data['user_id'])->id, ['revoked' => 1], true);
+
+        /**
+         * Create new auth code entry
+         */
+        $this->repository->create([
+            'id' => $id,
+            'user_id' => $data['user_id'],
+            'client_id' => $data['client_id'],
+            'scopes' => $data['scopes'],
+            'revoked' => 0,
+            'expires_at' => CarbonImmutable::now()->addMinutes(5)
+        ]);
+
+        return $jwt;
+    }
+}
