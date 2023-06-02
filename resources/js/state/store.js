5f8073e4329c8d8f664ff9e4a65f5d38490aabe1
Isuru <isuru2003a@gmail.com>
2023-06-02 20:41:12 +1000

    Create new store to manage client

--- resources/js/state/store.js
+++ resources/js/state/store.js
@@ -1,10 +1,12 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import userReducer from "../state/user";
 import loaderReducer from "../state/loader"
+import clientReducer from "../state/client"
 
 export default configureStore({
     reducer: {
         user: userReducer,
+        client: clientReducer,
         loader: loaderReducer
     },
 })
