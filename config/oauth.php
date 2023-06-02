7be448f99210409a850fe29439201ab8d8f581fd
Isuru <isuru2003a@gmail.com>
2023-06-02 18:50:00 +1000

    Create OAuth2 config file

---
+++ config/oauth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+    /**
+     * RS256 Private key file
+     */
+    'private_key' => '',
+
+    /**
+     * RS256 Public key file
+     */
+    'public_key' => '',
+
+    /**
+     * Supported (enabled) grant types
+     */
+    'grant_types' => [
+        'code',
+    ]
+];
