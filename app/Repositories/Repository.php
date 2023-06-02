ab55b8a563c78445ea84257a8dbbdaf66f4212c7
Isuru <isuru2003a@gmail.com>
2023-06-02 18:34:17 +1000

    Better record not found exception message

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -61,7 +61,7 @@ abstract class Repository implements RepositoryInterface
         try {
             $entry = $this->getBuilder()->findOrFail($id);
         } catch (ModelNotFoundException) {
-            throw new Exception('Record not found', 404);
+            throw new Exception('Record not found with id: ' . $id, 404);
         }
 
         return $entry;
