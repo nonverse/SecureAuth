248ad42abe37963c0ff39ee4ba6b1d54acf90f19
Isuru <isuru2003a@gmail.com>
2023-06-07 04:22:11 +1000

    Add logic to validate access token request if the client is requesting access token using refresh token

--- app/Http/Controllers/OAuth2/AbstractOAuth2Controller.php
+++ app/Http/Controllers/OAuth2/AbstractOAuth2Controller.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers\OAuth2;
 
 use App\Contracts\Repository\OAuth2\AuthCodeRepositoryInterface;
 use App\Contracts\Repository\OAuth2\ClientRepositoryInterface;
+use App\Contracts\Repository\OAuth2\RefreshTokenRepositoryInterface;
 use App\Contracts\Repository\OAuth2\ScopeRepositoryInterface;
 use App\Http\Controllers\Controller;
 use Exception;
 use Firebase\JWT\ExpiredException;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rule;
 
 class AbstractOAuth2Controller extends Controller
 {
@@ -32,15 +35,22 @@ class AbstractOAuth2Controller extends Controller
      */
     private AuthCodeRepositoryInterface $authCodeRepository;
 
+    /**
+     * @var RefreshTokenRepositoryInterface
+     */
+    private RefreshTokenRepositoryInterface $refreshTokenRepositoryInterface;
+
     public function __construct(
-        ClientRepositoryInterface   $clientRepository,
-        ScopeRepositoryInterface    $scopeRepository,
-        AuthCodeRepositoryInterface $authCodeRepository
+        ClientRepositoryInterface       $clientRepository,
+        ScopeRepositoryInterface        $scopeRepository,
+        AuthCodeRepositoryInterface     $authCodeRepository,
+        RefreshTokenRepositoryInterface $refreshTokenRepository,
     )
     {
         $this->clientRepository = $clientRepository;
         $this->scopeRepository = $scopeRepository;
         $this->authCodeRepository = $authCodeRepository;
+        $this->refreshTokenRepositoryInterface = $refreshTokenRepository;
     }
 
     /**
@@ -104,10 +114,11 @@ class AbstractOAuth2Controller extends Controller
     {
         $validator = Validator::make($request->all(), [
             'grant_type' => 'required',
-            'code' => 'required',
-            'redirect_uri' => 'required',
+            'code' => 'required_without:refresh_token',
+            'refresh_token' => 'required_without:code',
+            'redirect_uri' => 'required_without:refresh_token',
             'client_id' => 'required',
-            'scope' => 'required'
+            'scope' => 'required_without:refresh_token'
         ]);
 
         if ($validator->fails()) {
@@ -115,37 +126,43 @@ class AbstractOAuth2Controller extends Controller
         }
 
         /**
-         * Attempt to decode JWT
+         * This code will only run if the client is attempting to get an access token using an
+         * authorization code
          */
-        try {
-            $jwt = (array)JWT::decode($request->input('code'), new Key(config('oauth.public_key'), 'RS256'));
-        } catch (ExpiredException $e) {
-            return response()->json([
-                'error' => 'invalid_grant',
-                'error_description' => 'Authorization code has expired'
-            ], 400);
-        }
+        if ($request->input('code')) {
+            /**
+             * Attempt to decode JWT
+             */
+            try {
+                $jwtCode = (array)JWT::decode($request->input('code'), new Key(config('oauth.public_key'), 'RS256'));
+            } catch (ExpiredException $e) {
+                return response()->json([
+                    'error' => 'invalid_grant',
+                    'error_description' => 'Authorization code has expired'
+                ], 400);
+            }
 
-        $code = $this->authCodeRepository->get($jwt['jti']);
+            $code = $this->authCodeRepository->get($jwtCode['jti']);
 
-        /**
-         * Check if the authorization code has been revoked
-         */
-        if ($code->revoked) {
-            return response()->json([
-                'error' => 'invalid_grant',
-                'error_description' => 'Authorization code is invalid'
-            ], 400);
-        }
+            /**
+             * Check if the authorization code has been revoked
+             */
+            if ($code->revoked) {
+                return response()->json([
+                    'error' => 'invalid_grant',
+                    'error_description' => 'Authorization code is invalid'
+                ], 400);
+            }
 
-        /**
-         * Check that the redirect uri is the same as the one that was authorized
-         */
-        if ($request->input('redirect_uri') !== $jwt['aud']) {
-            return new JsonResponse([
-                'error' => 'invalid_grant',
-                'error_description' => 'Failed to verify redirect_uri'
-            ], 400);
+            /**
+             * Check that the redirect uri is the same as the one that was authorized
+             */
+            if ($request->input('redirect_uri') !== $jwtCode['aud']) {
+                return new JsonResponse([
+                    'error' => 'invalid_grant',
+                    'error_description' => 'Failed to verify redirect_uri'
+                ], 400);
+            }
         }
 
         /**
@@ -161,13 +178,47 @@ class AbstractOAuth2Controller extends Controller
         }
 
         /**
-         * Check if the requested client is the same as the one that was authorized
+         * This code will only run if the client is attempting to get an access token using an
+         * authorization code
          */
-        if ($client->id !== $code->client_id) {
-            return new JsonResponse([
-                'error' => 'invalid_client',
-                'error_description' => 'Client authentication failed'
-            ], 401);
+        if ($request->input('code')) {
+            /**
+             * Check if the requested client is the same as the one that was authorized
+             */
+            if ($client->id !== $code->client_id) {
+                return new JsonResponse([
+                    'error' => 'invalid_client',
+                    'error_description' => 'Client authentication failed'
+                ], 401);
+            }
+
+            /**
+             * If the client is authenticating via PKCE, verify the code_verifier
+             */
+            if (array_key_exists('cha', $jwtCode)) {
+                if (!$request->input('code_verifier')) {
+                    return new JsonResponse([
+                        'error' => 'invalid_request',
+                        'error_description' => 'code_verifier is required for authorization via PKCE'
+                    ], 400);
+                }
+                if (!Hash::check($request->input('code_verifier'), $jwtCode['cha'])) {
+                    return new JsonResponse([
+                        'error' => 'invalid_client',
+                        'error_description' => 'Client authentication failed'
+                    ], 401);
+                }
+            }
+
+            /**
+             * Check that the scopes are the same as the those that were authorized
+             */
+            if ($request->input('scope') !== $code->scopes) {
+                return new JsonResponse([
+                    'error' => 'invalid_scope',
+                    'error_description' => 'Failed to verify scope(s)'
+                ], 400);
+            }
         }
 
         /**
@@ -188,41 +239,41 @@ class AbstractOAuth2Controller extends Controller
             }
         }
 
-        /**
-         * If the client is authenticating via PKCE, verify the code_verifier
-         */
-        if (array_key_exists('cha', $jwt)) {
-            if (!$request->input('code_verifier')) {
-                return new JsonResponse([
-                    'error' => 'invalid_request',
-                    'error_description' => 'code_verifier is required for authorization via PKCE'
+        if ($request->input('refresh_token')) {
+
+            /**
+             * Attempt to decode JWT and get refresh token entry
+             */
+            try {
+                $jwtRefreshToken = (array)JWT::decode($request->get('refresh_token'), new Key(config('oauth.public_key'), 'RS256'));
+                $refreshToken = $this->refreshTokenRepositoryInterface->get($jwtRefreshToken['jti']);
+            } catch (ExpiredException $e) {
+                return response()->json([
+                    'error' => 'invalid_grant',
+                    'error_description' => 'Refresh token has expired'
+                ], 400);
+            } catch (ModelNotFoundException $e) {
+                return response()->json([
+                    'error' => 'invalid_grant',
+                    'error_description' => 'Refresh token is invalid'
                 ], 400);
             }
-            if (!Hash::check($request->input('code_verifier'), $jwt['cha'])) {
-                return new JsonResponse([
-                    'error' => 'invalid_client',
-                    'error_description' => 'Client authentication failed'
-                ], 401);
+
+            if ($refreshToken->revoked) {
+                return response()->json([
+                    'error' => 'invalid_grant',
+                    'error_description' => 'Refresh token is invalid'
+                ], 400);
             }
         }
 
         /**
-         * Check that a supported grant type is requested
+         * Check that a requested grant type is supported
          */
         if ($request->input('grant_type') !== 'authorization_code') {
             return new JsonResponse([
                 'error' => 'unsupported_grant_type',
                 'error_description' => 'Unsupported grant type'
-            ], 400);
-        }
-
-        /**
-         * That that the scopes are the same as the those that were authorized
-         */
-        if ($request->input('scope') !== $code->scopes) {
-            return new JsonResponse([
-                'error' => 'invalid_scope',
-                'error_description' => 'Failed to verify scope(s)'
             ], 400);
         }
     }
