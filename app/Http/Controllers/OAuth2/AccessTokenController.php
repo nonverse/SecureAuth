035ced4d2fe3420a1129e7f1eaf2c730ea2820a7
Isuru <isuru2003a@gmail.com>
2023-06-07 02:59:11 +1000

    Automatically issue refresh token if this is first time an access token is being issued for that client and user pair

--- app/Http/Controllers/OAuth2/AccessTokenController.php
+++ app/Http/Controllers/OAuth2/AccessTokenController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\OAuth2;
 
+use App\Contracts\Repository\OAuth2\AccessTokenRepositoryInterface;
 use App\Contracts\Repository\OAuth2\AuthCodeRepositoryInterface;
 use App\Contracts\Repository\OAuth2\ClientRepositoryInterface;
 use App\Contracts\Repository\OAuth2\ScopeRepositoryInterface;
 use App\Services\OAuth\AccessToken\CreateAccessTokenService;
+use App\Services\OAuth\RefreshToken\CreateRefreshTokenService;
 use Exception;
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
@@ -14,6 +16,11 @@ use Illuminate\Http\Request;
 
 class AccessTokenController extends AbstractOAuth2Controller
 {
+    /**
+     * @var AccessTokenRepositoryInterface
+     */
+    private AccessTokenRepositoryInterface $accessTokenRepository;
+
     /**
      * @var AuthCodeRepositoryInterface
      */
@@ -24,15 +31,24 @@ class AccessTokenController extends AbstractOAuth2Controller
      */
     private CreateAccessTokenService $createAccessTokenService;
 
+    /**
+     * @var CreateRefreshTokenService
+     */
+    private CreateRefreshTokenService $createRefreshTokenService;
+
     public function __construct(
-        ClientRepositoryInterface $clientRepository,
-        ScopeRepositoryInterface  $scopeRepository,
-        AuthCodeRepositoryInterface $authCodeRepository,
-        CreateAccessTokenService  $createAccessTokenService,
+        AccessTokenRepositoryInterface $accessTokenRepository,
+        ClientRepositoryInterface      $clientRepository,
+        ScopeRepositoryInterface       $scopeRepository,
+        AuthCodeRepositoryInterface    $authCodeRepository,
+        CreateAccessTokenService       $createAccessTokenService,
+        CreateRefreshTokenService      $createRefreshTokenService,
     )
     {
+        $this->accessTokenRepository = $accessTokenRepository;
         $this->authCodeRepository = $authCodeRepository;
         $this->createAccessTokenService = $createAccessTokenService;
+        $this->createRefreshTokenService = $createRefreshTokenService;
         parent::__construct($clientRepository, $scopeRepository, $authCodeRepository);
     }
 
@@ -67,16 +83,27 @@ class AccessTokenController extends AbstractOAuth2Controller
          */
         $token = $this->createAccessTokenService->handle($request, $code->user_id);
 
+        $response = [
+            'access_token' => $token['value'],
+            'token_type' => 'Bearer',
+            'expires_in' => $token['expires_in'],
+            'scope' => $request->input('scope')
+        ];
+
+        /**
+         * If this is the first time that the client and user pair are requesting an access token,
+         * automatically issue a long lasting refresh token
+         */
+        if (!$this->accessTokenRepository->getBuilder()->whereNot('id', $token['id'])->exists()) {
+            $refreshToken = $this->createRefreshTokenService->handle($request, $this->accessTokenRepository->get($token['id']));
+            $response['refresh_token'] = $refreshToken['value'];
+        }
+
         /**
          * Invalidate the authorization code that was used
          */
         $this->authCodeRepository->update($code->id, ['revoked' => 1]);
 
-        return new JsonResponse([
-            'access_token' => $token['value'],
-            'token_type' => 'Bearer',
-            'expires_in' => $token['expires_in'],
-            'scope' => $request->input('scope')
-        ]);
+        return new JsonResponse($response);
     }
 }
