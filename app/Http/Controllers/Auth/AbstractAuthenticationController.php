06ebd619e644cf3dc39f6aeb72f5f455d49c0dfc
Isuru <isuru2003a@gmail.com>
2023-06-08 23:15:25 +1000

    Remove parent cookie on logout

--- app/Http/Controllers/Auth/AbstractAuthenticationController.php
+++ app/Http/Controllers/Auth/AbstractAuthenticationController.php
@@ -9,6 +9,7 @@ use Carbon\CarbonInterface;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\Facades\Validator;
 
 class AbstractAuthenticationController extends Controller
@@ -70,11 +71,13 @@ class AbstractAuthenticationController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
+        $cookie = cookie('user_session', null, null, null, env('SESSION_PARENT_DOMAIN'));
+
         return response()->json([
             'data' => [
                 'success' => true
             ]
-        ])->withoutCookie('user');
+        ])->withoutCookie('user')->withCookie($cookie);
     }
 
 
