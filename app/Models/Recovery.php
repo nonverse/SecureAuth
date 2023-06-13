69eb75f1516efee31fa7be48194fde541a9ed182
Isuru <isuru2003a@gmail.com>
2023-06-13 23:42:16 +1000

    Create recovery model

---
+++ app/Models/Recovery.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Recovery extends Model
+{
+    use HasFactory;
+
+    public $table = 'recovery';
+
+    /**
+     * The primary key associated with the table
+     *
+     * @var string
+     */
+    protected $primaryKey = 'uuid';
+
+    /**
+     * Indicates if the model's primary key is auto-incrementing
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * Data type of the model's primary key
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+}
