7495d854127758363d88e9dda8bc9968ae9ddbc7
Isuru <isuru2003a@gmail.com>
2023-06-14 17:11:30 +1000

    Add backend post logic and error handling to Authorize view

--- resources/js/components/Authorize/Password.jsx
+++ resources/js/components/Authorize/Password.jsx
@@ -3,22 +3,56 @@ import {Formik} from "formik";
 import Form from "../../elements/Form";
 import Field from "../../elements/Field";
 import validate from "../../scripts/validate";
+import {auth} from "../../scripts/api/auth";
+import {useState} from "react";
+import {useDispatch} from "react-redux";
+import {updateLoader} from "../../state/loader";
 
 const Password = () => {
+
+    const query = new URLSearchParams(window.location.search)
+    const [error, setError] = useState('')
+    const dispatch = useDispatch()
+
+    function validatePassword(value) {
+        setError('')
+        return validate.require(value)
+    }
 
     return (
         <Fluid heading="Authorization Required" subHeading="Update E-Mail">
             <Formik initialValues={{
                 password: ''
-            }} onSubmit={(values) => {
-
+            }} onSubmit={async (values) => {
+                dispatch(updateLoader(true))
+                await auth.post('authorize', {
+                    ...values,
+                    action_id: query.get('action_id')
+                })
+                    .then(response => {
+                        if (response.data.data.authorized) {
+                            window.location = `https://${query.get('host')}${query.get('resource')}?authorization_token=${response.data.data.authorization_token}`
+                        }
+                    })
+                    .catch(e => {
+                        switch (e.response.status) {
+                            case 401:
+                                setError('Password is incorrect')
+                                break
+                            default:
+                                setError('Something went wrong')
+                        }
+                        dispatch(updateLoader(false))
+                    })
             }}>
                 {({errors}) => (
                     <Form id="fluid-form" cta="Continue">
-                        <Field password name="password" label="Password" validate={validate.require} error={errors.password}/>
+                        <Field password name="password" label="Password" validate={validatePassword}
+                               error={errors.password ? errors.password : error}/>
                         <div className="fluid-text">
                             <p>
-                                For your security, we need to verify that it's really you who is making this change to your account
+                                For your security, we need to verify that it's really you who is making this change to
+                                your account
                             </p>
                         </div>
                     </Form>
