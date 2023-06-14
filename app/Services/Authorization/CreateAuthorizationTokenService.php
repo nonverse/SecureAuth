7339187a99c3c44d35afd4b681c85f7066869148
Isuru <isuru2003a@gmail.com>
2023-06-14 16:48:17 +1000

    Create service to generate authorization tokens

---
+++ app/Services/Authorization/CreateAuthorizationTokenService.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Services\Authorization;
+
+use App\Contracts\Repository\AuthorizationTokenRepositoryInterface;
+use Carbon\CarbonImmutable;
+use Firebase\JWT\JWT;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class CreateAuthorizationTokenService
+{
+    /**
+     * @var AuthorizationTokenRepositoryInterface
+     */
+    private AuthorizationTokenRepositoryInterface $repository;
+
+    public function __construct(
+        AuthorizationTokenRepositoryInterface $repository
+    )
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * Create a new authorization token and entry
+     *
+     * @param Request $request
+     * @param array $data
+     * @return string
+     */
+    public function handle(Request $request, array $data): string
+    {
+        $id = Str::random(100);
+
+        $payload = [
+            'iss' => env('APP_URL'),
+            'aud' => env('VITE_ACCOUNT_APP'),
+            'iat' => time(),
+            'exp' => time() + 5 * 60,
+            'jti' => $id
+        ];
+
+        /**
+         * Create new authorization token
+         */
+        $jwt = JWT::encode($payload, config('oauth.private_key'), 'RS256');
+
+        /**
+         * Create new authorization token entry
+         */
+        $this->repository->create([
+            'id' => $id,
+            'user_id' => $request->user()->uuid,
+            'action_id' => $data['action_id'],
+            'expires_at' => CarbonImmutable::now()->addMinutes(5),
+        ]);
+
+        return $jwt;
+    }
+}
