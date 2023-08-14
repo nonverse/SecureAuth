89a35ec59893931de1c72a505e4f8ffc2787bbc7
Isuru <isuru2003a@gmail.com>
2023-08-15 01:15:28 +1000

    Add logic to return user to account app if they are automatically logged in

--- resources/js/components/SwitchUser.jsx
+++ resources/js/components/SwitchUser.jsx
@@ -9,8 +9,11 @@ const SwitchUser = () => {
         async function handle() {
             await axios.post('https://auth.nonverse.test/switch-user', query)
                 .then(response => {
-                    if (response.data.success) {
-                        window.location = response.data.data.redirect_uri
+                    if (response.data.complete) {
+                        if (response.data.data.redirect_uri) {
+                            return window.location = response.data.data.redirect_uri
+                        }
+                        return window.location = `https://${query.get('host') ? query.get('host') : 'account.nonverse.test'}${query.get('resource') ? query.get('resource') : '/'}`
                     }
                 })
                 .catch(e => {
