7cc86d6f0552f447f0c0383e21fb8afb4e2fcc0a
isuru <isuru2003a@gmail.com>
2023-08-15 23:45:27 +1000

    Create UserManagementService

---
+++ app/Services/User/UserManagementService.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace App\Services\User;
+
+use App\Contracts\Repository\SettingsRepositoryInterface;
+use App\Models\User;
+use Carbon\CarbonImmutable;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Cookie\CookieJar;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+
+class UserManagementService
+{
+    /**
+     * @var SettingsRepositoryInterface
+     */
+    private SettingsRepositoryInterface $settingsRepository;
+
+    /**
+     * @var array
+     */
+    private array $updatedCookie;
+
+    public function __construct(
+        SettingsRepositoryInterface $settingsRepository
+    )
+    {
+        $this->settingsRepository = $settingsRepository;
+        $this->updatedCookie = [];
+    }
+
+    /**
+     * Add a user to the local user cookie
+     * This method should only be called when a user has logged themselves
+     * in using their password or any other sort of login challenge
+     *
+     * @param Request $request
+     * @param User $user
+     * @return void
+     */
+    public function add(Request $request, User $user): void
+    {
+        /**
+         * Get the user cookie from request
+         */
+        $cookie = $this->getCookie($request);
+
+        /**
+         * Get user's settings
+         */
+        $userSettings = [];
+        foreach ($this->settingsRepository->getUserSettings($user->uuid) as $setting) {
+            $userSettings[$setting['key']] = $setting['value'];
+        }
+
+
+        /**
+         * Create new entry for user
+         */
+        $cookie[$user->uuid] = [
+            'session' => [
+                'aat' => CarbonImmutable::now(),
+            ],
+            'settings' => [
+                'thm' => array_key_exists('theme', $userSettings) ? $userSettings['theme'] : 'system',
+                'lan' => array_key_exists('language', $userSettings) ? $userSettings['language'] : 'en-AU'
+            ]
+        ];
+
+        $this->updatedCookie = $cookie;
+    }
+
+    /**
+     * Remove a user from the local user cookie
+     * This will remove all their session data that was stored in that cookie
+     *
+     * @param Request $request
+     * @param User $user
+     * @return void
+     */
+    public function remove(Request $request, User $user): void
+    {
+        /**
+         * Get user cookie from request
+         */
+        $cookie = $this->getCookie($request);
+
+        /**
+         * Remove user from cookie
+         */
+        if (array_key_exists($user->uuid, $cookie)) {
+            unset($cookie[$user->uuid]);
+        }
+
+        $this->updatedCookie = $cookie;
+    }
+
+    /**
+     * Add expiry to user cookie entry
+     * This method should be called when a user is being timed out.
+     * The user will be considered inactive and can login without
+     * being challenged until the value in ['session']['exp']
+     *
+     * @param Request $request
+     * @param User $user
+     * @return void
+     */
+    public function remember(Request $request, User $user): void
+    {
+        $cookie = $this->getCookie($request);
+
+        $cookie[$user->uuid] = [
+            'session' => [
+                ...$cookie[$user->uuid]['session'],
+                'exp' => CarbonImmutable::now()->addMinutes(config('auth.user_session.remember'))
+            ],
+            'settings' => [
+                ...$cookie[$user->uuid]['settings']
+            ]
+        ];
+
+        $this->updatedCookie = $cookie;
+    }
+
+    /**
+     * Get the cookie to attach to outgoing request
+     *
+     * @return Application|CookieJar|\Illuminate\Foundation\Application|Cookie
+     */
+    public function getResponseCookie(): \Illuminate\Foundation\Application|CookieJar|Cookie|Application
+    {
+        return cookie('user', json_encode($this->updatedCookie));
+    }
+
+    /**
+     * Get the current user cookie
+     *
+     * @param Request $request
+     * @return array
+     */
+    protected function getCookie(Request $request): array
+    {
+        if (empty($this->updatedCookie)) {
+            $this->updatedCookie = $request->cookie('user') ? json_decode($request->cookie('user'), true) : [];
+        }
+
+        return $this->updatedCookie;
+    }
+}
