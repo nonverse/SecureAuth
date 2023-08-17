e7a7c9e16112b95466b04975c97983a44be750f4
isuru <isuru2003a@gmail.com>
2023-08-17 21:38:03 +1000

    If user has valid session attempt to log them in

--- resources/js/components/Login/AccountSelector.jsx
+++ resources/js/components/Login/AccountSelector.jsx
@@ -8,6 +8,7 @@ import {updateUser} from "../../state/user";
 const AccountSelector = ({restart}) => {
 
     const users = useSelector(state => state.users.value)
+    const query = new URLSearchParams(window.location.search)
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
@@ -15,14 +16,30 @@ const AccountSelector = ({restart}) => {
         <Fluid heading="Welcome back" subHeading="Choose an account">
             {Object.keys(users).map((user) => (
                 <Card key={user} name={`${users[user]['data']['name_first']} ${users[user]['data']['name_last']}`}
-                      value={users[user]['data']['email']} onClick={() => {
-                    dispatch(updateUser({
-                        email: users[user]['data']['email'],
-                        name_first: users[user]['data']['name_first'],
-                        name_last: users[user]['data']['name_last']
-                    }))
-                    restart()
-                }}/>
+                      value={users[user]['data']['email']}
+                      info={users[user]['session']['is_valid'] ? 'Logged In' : ''}
+                      onClick={async () => {
+                          if (users[user]['session']['is_valid']) {
+                              await axios.post(`https://auth.nonverse.test/login`, {
+                                  uuid: user
+                              }, {
+                                  withCredentials: true
+                              })
+                                  .then(response => {
+                                      if (response.data.complete) {
+                                          return window.location = `https://${query.get('host') ? query.get('host') : 'account.nonverse.test'}${query.get('resource') ? query.get('resource') : '/'}`
+                                      }
+                                      dispatch(updateUser(response.data.data))
+                                      restart()
+                                  })
+                          }
+                          dispatch(updateUser({
+                              email: users[user]['data']['email'],
+                              name_first: users[user]['data']['name_first'],
+                              name_last: users[user]['data']['name_last']
+                          }))
+                          restart()
+                      }}/>
             ))}
             <InLineButton id="add-account" onClick={() => {
                 navigate('/')
